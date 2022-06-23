31fc7a54e01bef7819cff44bdad9fb71ce8bc1f2
Rita Chien <ritachien929@gmail.com>
2022-06-23 17:47:14 +0800

    feat: add users router module

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router()
 
 // Includes routers module
 const home = require('./modules/home')
+const users = require('./modules/users')
 
 router.use('/', home)
+router.use('/users', users)
 
 // Export module
 module.exports = router
