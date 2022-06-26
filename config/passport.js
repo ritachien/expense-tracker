8f2b2eb20ecbc6173bacfa205d035ef4980a5dda
Rita Chien <ritachien929@gmail.com>
2022-06-26 16:13:29 +0800

    feat: add google login/register feature

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const FacebookStrategy = require('passport-facebook')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
@@ -56,6 +57,32 @@ module.exports = app => {
       })
       return done(null, newUser)
     }
+  ))
+
+  // Set Google Strategy
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_clientID,
+    clientSecret: process.env.GOOGLE_clientSecret,
+    callbackURL: process.env.GOOGLE_callbackURL,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user)
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(err => done(err))
+      })
+  }
   ))
 
   // Serialize and deserialize
--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -13,4 +13,14 @@ router.get('/facebook/callback', passport.authenticate('facebook', {
   failureRedirect: '/users/login'
 }))
 
+// Route for Google auth ===========================
+router.get('/google', passport.authenticate('google', {
+  scope: ['email', 'profile']
+}))
+
+router.get('/google/callback', passport.authenticate('google', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
 module.exports = router
