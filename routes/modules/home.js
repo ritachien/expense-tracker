cae5bc570acabf9e945030c1ac0a62fae515329e
Rita Chien <ritachien929@gmail.com>
2022-06-26 12:30:02 +0800

    feat: render records by selected category

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,17 +6,18 @@ const dayjs = require('dayjs')
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
+// Render all record items
 router.get('/', async (req, res) => {
   try {
     let totalAmount = 0
     const userId = req.user._id
-    // Get category data from database
+    // Get all category data from database
     const categoryFromDb = await Category.find().lean()
     const category = {}
     categoryFromDb.forEach(item => category[item._id] = item)
 
     // Get records from database and
-    const recordsFromDb = await Record.find({ userId }).lean().sort('-id')
+    const recordsFromDb = await Record.find({ userId }).lean()
     const records = []
     recordsFromDb.forEach(item => {
       //  Get related icon
@@ -29,11 +30,38 @@ router.get('/', async (req, res) => {
     })
 
     // Render page
-    res.render('index', { totalAmount, records })
+    res.render('index', { totalAmount, records, categoryFromDb })
 
   } catch (err) {
     console.log(err)
   }
+})
+
+// Render records by selected category
+router.post('/', async (req, res) => {
+  let totalAmount = 0
+  const userId = req.user._id
+  const categoryId = req.body.categorySelect
+  // Get all category data from database
+  const categoryFromDb = await Category.find().lean()
+  const category = {}
+  categoryFromDb.forEach(item => category[item._id] = item)
+  const categorySelected = category[categoryId].name
+  // Get records from database and
+  const recordsFromDb = await Record.find({ userId, categoryId }).lean()
+  const records = recordsFromDb
+  records.forEach(item => {
+    //  Get related icon
+    item.icon = category[item.categoryId].icon
+    // Format date
+    item.date = dayjs(item.date).format('YYYY-MM-DD')
+    totalAmount += item.amount
+  })
+
+  // Render page
+  res.render('index', { totalAmount, records, categoryFromDb, categorySelected })
+
+
 })
 
 // Export module
